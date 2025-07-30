cb9ee24de7596ed8ede3c1317d0970a62617a27d
Justin Parisio <114594154+jparisio@users.noreply.github.com>
2025-07-30 10:56:00 -0400

    lenis

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,41 @@ import type { AppProps } from "next/app";
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
 import NavBar from "@/components/NavBar";
+import { useEffect } from "react";
+import Lenis from "lenis";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis({
+      autoRaf: true,
+    });
+
+    // Listen for the scroll event and log the event data
+    lenis.on("scroll", (e) => {
+      console.log(e);
+    });
+
+    // Cleanup function
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <>
+      <NavBar />
       <AnimatePresence mode="wait">
-        <motion.div key={router.asPath}>
-          <NavBar />
-
+        <motion.div
+          key={router.asPath}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        >
           <Component {...pageProps} />
         </motion.div>
       </AnimatePresence>
